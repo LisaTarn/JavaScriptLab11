926ccfa483249df96b2fd26e994828d1ff9dfb7a
LisaTarn <lisatarnowpt@gmail.com>
2024-12-07 12:29:03 -0500

    added static method applyDiscount and fixed up bugs

--- script.js
+++ script.js
@@ -10,11 +10,13 @@ class productProperties{
     }
 
     toString(){
-        console.log(`Product: ${this.product}, Price: ${this.price}, Quantity: ${this.quantity}` )
+        console.log('Product: ${this.product}, Price: ${this.price}, Quantity: ${this.quantity}' )
     }
 
     static applyDiscount(products, discount){
-        return products*discount
+        this.products = products;
+        this.price = price;
+        return price - (price*discount);
     }
 }
 
@@ -38,5 +40,17 @@ let cheese = new PerishableProductProperties("cheese", 17.00, 1, "2024-12-29");
 cheese.getTotalValue();
 cheese.toString();
 
+class Product{
+    static applyDiscount(products, discount){
+        const price = products.map(item => item.price);
+        return price.map(price => price - (price*discount));
+}}
+
+let products = [
+    new productProperties("orange", 2.50, 3),
+    new productProperties("apple", 3.00, 7)
+];
+
+console.log(Product.applyDiscount(products, 0.1));
 
 
